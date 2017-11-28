3b93269aad78e1fd9640a995dfdae002a4224fb0
ellen-xu <33074674+ellen-xu@users.noreply.github.com>
2017-11-28 16:07:10 -0500

    final touches for milestone3

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <div class="footer">
     <footer>
-      <p class="footer-caption">Email us: <a id="footer-email" href="mailto:rm686@cornell.edu?Subject=Potential%20Partnership">rm686@cornell.edu</a></p>
+      <p class="footer-caption1">Email us: <a id="footer-email" href="mailto:rm686@cornell.edu?Subject=Potential%20Partnership">rm686@cornell.edu</a></p>
 
-      <p class="footer-caption">Meeting Location: Kennedy Hall Room 213, Cornell University, Ithaca, NY</p>
+      <p class="footer-caption2">Meeting Location: Kennedy Hall Room 213, Cornell University, Ithaca, NY</p>
 
       <!-- <p class="footer-caption">**Roadways &#38; walkways consist of grass, stone/gravel or blacktop.</p> -->
     </footer>
--- mission.php
+++ mission.php
@@ -12,93 +12,94 @@
   <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
 </head>
 <body>
-<?php include "includes/navigation.php"; ?>
-<div id="containEd">
-  <h1>Impact Investing</h1>
+  <?php include "includes/navigation.php"; ?>
+  <div id="containEd">
+    <h1>Impact Investing</h1>
 
 
-<div class="impactinvestinginfo">
-    <div id= "aboutimpact">
-
-    <h2>Who Are We?</h2>
-      <p> Given our global political climate, it has become more important
-        now than ever before to understand and take responsibility for the
-         implications of our actions as citizens of the world. I strongly
-          believe that impact investing allows people the opportunity to
-           do just that, supporting ventures that create paths toward a
-           healthier planet. Impact investing though projected to be the
-           upcoming trend within the world of finance, really hasn’t reached
+    <div class="impactinvestinginfo">
+      <div id= "aboutimpact">
+        <div id=rahulquote>
+          <h2>Who Are We?</h2>
+          <p> "Given our global political climate, it has become more important
+            now than ever before to understand and take responsibility for the
+            implications of our actions as citizens of the world. I strongly
+            believe that impact investing allows people the opportunity to
+            do just that, supporting ventures that create paths toward a
+            healthier planet. Impact investing though projected to be the
+            upcoming trend within the world of finance, really hasn’t reached
             the mainstream yet. Thus our purpose of founding CII, one of the
             only impact investing undergraduate clubs in the country, was to
             take on a pioneering role and allow more people the opportunity
             to learn and get involved in impact investing. In this way, our
             hope for CII is to not only bring in people with prior finance
             experience, but to expand the scope of this niche to people of
-            different backgrounds and skillsets. </p>
+            different backgrounds and skillsets." </p>
             <p><i>- Rahul Mukherjee, Chief Executive Officer</i></p>
-    <!-- <div id="definition">
-      <h2 id="deftitle">Impact Investment</h2>
-      <p id="defpronounce">[im-pakt in-vest-muh nt]</p>
-      <p>NOUN: Impact investing is investing that aims to generate specific beneficial social
-        or environmental effects in addition to financial gain.</p>
-      </div> -->
-        <h2>What is Impact Investing?</h2>
-<div class="centEd">
-      <p>Impact investments are investments made into companies, organizations,
-        and funds with the intention to generate social and environmental impact
-        alongside a financial return. Impact investments can be made in both emerging
-        and developed markets, and target a range of returns from below market to market rate,
-        depending on investors' strategic goals. </p>
-        <img class="aboutimpact" alt= "aboutimpact" src="images/aboutimpact.jpg">
-
-
-      <div id= "whyimpact">
-        <h2>Why Impact Investing?</h2>
-        <p>Impact investing challenges the long-held views
-          that social and environmental issues should be addressed only by philanthropic donations,
-          and that market investments should focus exclusively on achieving financial returns.</p>
-
-          <p>The impact investing market offers diverse and viable opportunities for investors
-            to advance social and environmental solutions through investments that also produce
-            financial returns. </p>
-            <img class="aboutimpact" alt= "whyimpact" src="images/whyimpact.jpg">
           </div>
-        </div>
-</div>
-<div class= "analystdevinfo">
-          <h1>Analyst Development Program</h1>
-          <h2>About</h2>
-          <p>The Analyst Development Program (ADP) is a 4-week long program,
-            with four 1 hour long lectures and 2 weekend workshops that
-            will give you real applicable knowledge and effective expertise.
-            By the end of the ADP, you will be able to construct comprehensive
-            Investment Proposal Decks,valuate companies, and present your ideas
-            in a coherent and applicable manner.</p>
-<div id="calender">
-    <div id="calender-left">
-            <h2>Lectures and Workshops</h2>
-            <ul>
-                <li class="calender-li"><span class="bold">Week 1, October 24nd:</span> Accounting and Financial Terms</li>
-                <li class="calender-li"><span class="bold">Week 2, October 31st:</span> How to Market Research</li>
-                <li class="calender-li"><span class="bold">Week 2, November 4th:</span> (Saturday) Investment-Presentation Workshop </li>
-                <li class="calender-li"><span class="bold">Week 3, November 7nd:</span> Valuations Part 1</li>
-                <li class="calender-li"><span class="bold">Week 3, November 11th:</span> (Saturday) Valuations Workshop Part 1</li>
-                <li class="calender-li"><span class="bold">Week 4, November 14th:</span> Valuations Part 2</li>
-                <li class="calender-li"><span class="bold">Week 4, November 18th:</span> (Saturday) DCF Workshop</li>
-              </ul>
-        </div>
-        <div id="calender-right">
-              <h2>Case Study</h2>
-              <ul>
-                <li class="calender-li"><span class="bold">Week 4, November 18th:</span> Impact Investment Case Preparation</li>
-                <li class="calender-li"><span class="bold">Final Week, December 4th:</span> Presentation</li>
-              </ul>
-      </div>
-      </div>
-    </div>
-  </div>
-</div>
-      <?php include "includes/footer.php"; ?>
-            </body>
+          <!-- <div id="definition">
+          <h2 id="deftitle">Impact Investment</h2>
+          <p id="defpronounce">[im-pakt in-vest-muh nt]</p>
+          <p>NOUN: Impact investing is investing that aims to generate specific beneficial social
+          or environmental effects in addition to financial gain.</p>
+        </div> -->
+        <h2>What is Impact Investing?</h2>
+        <div class="centEd">
+          <p>Impact investments are investments made into companies, organizations,
+            and funds with the intention to generate social and environmental impact
+            alongside a financial return. Impact investments can be made in both emerging
+            and developed markets, and target a range of returns from below market to market rate,
+            depending on investors' strategic goals. </p>
+            <img class="aboutimpact" alt= "aboutimpact" src="images/aboutimpact.jpg">
 
-            </html>
+
+            <div id= "whyimpact">
+              <h2>Why Impact Investing?</h2>
+              <p>Impact investing challenges the long-held views
+                that social and environmental issues should be addressed only by philanthropic donations,
+                and that market investments should focus exclusively on achieving financial returns.</p>
+
+                <p>The impact investing market offers diverse and viable opportunities for investors
+                  to advance social and environmental solutions through investments that also produce
+                  financial returns. </p>
+                  <img class="aboutimpact" alt= "whyimpact" src="images/whyimpact.jpg">
+                </div>
+              </div>
+            </div>
+            <div class= "analystdevinfo">
+              <h1>Analyst Development Program</h1>
+              <h2>About</h2>
+              <p>The Analyst Development Program (ADP) is a 4-week long program,
+                with four 1 hour long lectures and 2 weekend workshops that
+                will give you real applicable knowledge and effective expertise.
+                By the end of the ADP, you will be able to construct comprehensive
+                Investment Proposal Decks,valuate companies, and present your ideas
+                in a coherent and applicable manner.</p>
+                <div id="calender">
+                  <div id="calender-left">
+                    <h2>Lectures and Workshops</h2>
+                    <ul>
+                      <li class="calender-li"><span class="bold">Week 1, October 24nd:</span> Accounting and Financial Terms</li>
+                      <li class="calender-li"><span class="bold">Week 2, October 31st:</span> How to Market Research</li>
+                      <li class="calender-li"><span class="bold">Week 2, November 4th:</span> (Saturday) Investment-Presentation Workshop </li>
+                      <li class="calender-li"><span class="bold">Week 3, November 7nd:</span> Valuations Part 1</li>
+                      <li class="calender-li"><span class="bold">Week 3, November 11th:</span> (Saturday) Valuations Workshop Part 1</li>
+                      <li class="calender-li"><span class="bold">Week 4, November 14th:</span> Valuations Part 2</li>
+                      <li class="calender-li"><span class="bold">Week 4, November 18th:</span> (Saturday) DCF Workshop</li>
+                    </ul>
+                  </div>
+                  <div id="calender-right">
+                    <h2>Case Study</h2>
+                    <ul>
+                      <li class="calender-li"><span class="bold">Week 4, November 18th:</span> Impact Investment Case Preparation</li>
+                      <li class="calender-li"><span class="bold">Final Week, December 4th:</span> Presentation</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <?php include "includes/footer.php"; ?>
+        </body>
+
+        </html>
