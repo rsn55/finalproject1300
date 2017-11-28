521e0585bca89a86835c2f626f0853cc27d52ab5
JunanQu <33074554+JunanQu@users.noreply.github.com>
2017-11-27 21:49:03 -0500

    Updated slowly loading issue

--- scripts/slide.js
+++ scripts/slide.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	// the array of images
-	var images=['../images/cornell.jpg', 'images/slide1.jpg', 'images/slide3.jpg'];
+	var images=[ 'images/slide3.jpg', '../images/cornell.jpg', 'images/slide1.jpg'];
 	var piclink = ["url(" + images[0] + ")", "url(" + images[1] + ")", "url(" + images[2] + ")"];
 	// current image index
 	var backgroundImage = 0;
@@ -11,17 +11,17 @@ $(document).ready(function() {
 
   // var imgURL = "url(".concat(images[0]);
   // var imgURLnew = imgURL.concat(")");
-  document.body.style.backgroundImage = "url(" + images[1] + ")";
+  document.body.style.backgroundImage = "url(" + images[0] + ")";
 	//
 
-	var images=['../images/cornell.jpg','images/slide1.jpg', 'images/slide3.jpg'];
+	var images=[ 'images/slide3.jpg', '../images/cornell.jpg', 'images/slide1.jpg'];
 	var url=0;
-	beginNow = setInterval(function(){
+	setInterval(function(){
 	   url+=1;
 	  if(url==3){
 	    url=0;
 	  }
-	  document.body.style.backgroundImage = 'url('+images[url]+')';
+	  document.body.style.backgroundImage = 'url('+ images[url] +')';
 	  document.body.style.backgroundRepeat = "no-repeat";
 		document.body.style.backgroundWidth = "100%";
 		document.body.style.backgroundHeight = "100%";
