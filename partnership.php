d56c24d882aac68775f1d035ce4f3f712f48d5fd
Rachel Susanne Nash <rsn55@cornell.edu>
2017-11-28 03:45:01 -0500

    added partnership

--- partnership.php
+++ partnership.php
@@ -11,87 +11,72 @@
   <link rel="stylesheet" type="text/css" href="styles/all.css" media="all"/>
 </head>
 <body>
-<?php include "includes/navigation.php"; ?>
+  <?php include "includes/navigation.php"; ?>
 
-<div id="partner-div-2">
-  <h1 class="partner-title">Partnerships</h1>
-  <div id="calvert">
-    <div id="calv-border">
-    <img id="calvert-h1" class="partIm" src="images/boa-logo.jpg" alt="boa">
 
-    <div id="calvert-paragraph" class="descript">
-      <p id="exception3">Calvert Investments (Now Calvert Research and Management)
-        was founded in 1988 to offer investors more direct ways to invest in communities.
-         Working with individual investors has always
-        been part of their mission.
-        They were one of the first “impact investments” available through brokerage accounts.
-        They aim to make impact investing accessible and relevant to a diverse range of investors.
-      </p>
-      </div>
-    </div>
+
+  <div id="partner-div-1">
+
+    <h1 class="partner-title">Why Sponsor Us</h1>
+
+
+
+    <p class="wedo-paragraph">
+      By partnering with Cornell Impact Investing Club,
+      organizations gain access to professionally-trained
+      volunteer analysts. Sponsors help up-and-coming financial
+      analysts gain meaningful experience in the real world.
+      If you are interested in partnering with us, please contact
+      Rahul Mukherjee, our Chief Executive Officer, at <a href="mailto:rm686@cornell.edu?Subject=Potential%20Partnership">rm686@cornell.edu</a>.
+    </p>
+    <img id="lineDec" src="images/line.png" alt="line">
   </div>
 
 
-  <div id="jpmorgan">
-    <div id="impact-border">
-    <img id="impact-h1" class="partIm" src="images/jplogo.jpg" alt="jp">
 
-    <div id="impact-paragraph" class="descript">
-    <p id="exception2">   In 2010, Calvert Impact Capital created ImpactAssets
-      to assume responsibility for the Giving Fund and transform it into
-      something much bigger.
-      ImpactAssets is an independent 501(c)3 organization, but remains closely
-      connected to Calvert Impact Capital. Board members designated by Calvert
-      Impact Capital hold a minority position on ImpactAssets’ Board of
-      Directors and Community Investment Partners.
-    </p>
+  <div id="partner-div-2">
+    <h1 class="partner-title">Partnerships</h1>
+    <div id="calvert">
+      <div id="calv-border">
+        <img id="calvert-h1" class="partIm" src="images/boa-logo.jpg" alt="boa">
+
+        <div id="calvert-paragraph" class="descript">
+          <p id="exception3">Calvert Investments (Now Calvert Research and Management)
+            was founded in 1988 to offer investors more direct ways to invest in communities.
+            Working with individual investors has always
+            been part of their mission.
+            They were one of the first “impact investments” available through brokerage accounts.
+            They aim to make impact investing accessible and relevant to a diverse range of investors.
+          </p>
+        </div>
+      </div>
     </div>
+    <div id="jpmorgan">
+      <div id="impact-border">
+        <img id="impact-h1" class="partIm" src="images/jplogo.jpg" alt="jp">
+
+        <div id="impact-paragraph" class="descript">
+          <p id="exception2">   In 2010, Calvert Impact Capital created ImpactAssets
+            to assume responsibility for the Giving Fund and transform it into
+            something much bigger.
+            ImpactAssets is an independent 501(c)3 organization, but remains closely
+            connected to Calvert Impact Capital. Board members designated by Calvert
+            Impact Capital hold a minority position on ImpactAssets’ Board of
+            Directors and Community Investment Partners.
+          </p>
+        </div>
+      </div>
     </div>
-    </div>
-
-<div id="partner-div-1">
 
 
 
-<img class="lineDec" src="images/line.png" alt="line">
 
-<div id="quote-partner">
-  <p> Do not take life too seriously.</p>
-  <p> You will never get out of it alive.</p>
-  <p> --Elbert Hubbard<p>
+
+
+
+  </div>
+
 </div>
-
-
-<img class="lineDec" src="images/line-flip.png" alt="line">
-
-<h1 class="partner-title" id="partner-title-1">Why Sponsor Us</h1>
-
-
-
-<p>
-    By partnering with Cornell Impact Investing Club,
-    organizations gain access to professionally-trained
-    volunteer analysts.</p>
-<p class="wedo-paragraph"> Sponsors help up-and-coming financial analysts gain meaningful experience in the real world.</p>
-
-
-</div>
-
-
-
-
-
-
-
-
-
-
-
- </div>
-
- <?php include "includes/footer.php"; ?>
-
-
 
 
 
